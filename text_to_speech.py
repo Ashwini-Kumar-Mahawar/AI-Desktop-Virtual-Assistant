95fbaed7973482c0f54662c7da964a68eede96ad
Ashwini Kumar Mahawar <131292314+Ashwini-Kumar-Mahawar@users.noreply.github.com>
2025-03-12 16:28:29 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -4,7 +4,7 @@ def speak(text):
     # Initialize the text-to-speech engine
     engine = pyttsx3.init()
 
-    # Set properties (optional)
+    # Set properties
     engine.setProperty("rate", 150)  # Speed of speech
     engine.setProperty("volume", 1.0)  # Volume level (0.0 to 1.0)
 
